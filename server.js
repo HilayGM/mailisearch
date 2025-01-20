dfef4a98c215d33e102673f7dd7548728c95fa8e
HilayGM <hilailibreros@gmail.com>
2025-01-20 10:42:45 -0600

    prueba de mailisearch y conexion al front

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { MeiliSearch } = require('meilisearch');
+const cors = require('cors'); // Habilitar CORS para permitir solicitudes desde el frontend
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MeiliSearch({
+  host: 'http://172.233.131.83:7700',
+  apiKey: '6d9bb59dca80659707c17d1363e5393fd13475362644c14faf0652ee886',
+});
+
+// Endpoint para realizar la búsqueda
+app.post('/search', async (req, res) => {
+  try {
+    const { query } = req.body; // Obtener el término de búsqueda desde el cuerpo de la solicitud
+
+    if (!query || query.trim() === '') {
+      return res.status(400).json({ error: 'El término de búsqueda es obligatorio' });
+    }
+
+    const index = client.index('movies'); // Especificar el índice a buscar
+    const searchResults = await index.search(query, {
+      limit: 10, // Limitar los resultados a los primeros 10 documentos
+    });
+
+    res.status(200).json(searchResults);
+  } catch (error) {
+    console.error('Error en la búsqueda:', error);
+    res.status(500).json({ error: 'Hubo un problema al realizar la búsqueda' });
+  }
+});
+
+// Iniciar el servidor en el puerto 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
